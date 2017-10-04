0cc33edd3bbb71f432131442e750e33cb5a2b42c
lukkyjoe <lukkyjoe@macbookcbook459.lan>
2017-10-03 20:57:37 -0700

    Refactor to static serve.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@
  var webpack = require('webpack');
  var webpackDevMiddleware = require('webpack-dev-middleware');
  var webpackHotMiddleware = require('webpack-hot-middleware');
- var publicPath = path.resolve(__dirname, '/../client');
+ var publicPath = path.resolve(__dirname, '../client');
 
  var app = express();
 
@@ -13,12 +13,7 @@
  app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
  app.use(webpackHotMiddleware(compiler));
 
-
  app.use(express.static(publicPath));
-
- app.use('/', function (req, res) {
-   res.sendFile(path.resolve('client/index.html'));
- });
 
  var port = process.env.PORT || 3000;
 
